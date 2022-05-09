c9caf675f8a5c2ab227adf56a395eb2fef976e4a
tweep <vogelj4@gene.com>
2022-05-09 08:30:23 -0700

    - changes

--- sync_zshrc.sh
+++ sync_zshrc.sh
@@ -1,2 +1,4 @@
 #!/bin/zsh
-fswatch -o $HOME/.zshrc $HOME/.zsh/ | xargs -n1 -I{} echo "file changed"
+DIR_PATH=`dirname "$0"`
+/usr/local/bin/fswatch-o $HOME/.zshrc $HOME/.zsh/ | xargs -n1 zsh $DIR_PATH/commit_changes.sh
+#fswatch -o $HOME/.zshrc $HOME/.zsh/ | xargs -n1 -I{} echo "file changed"
